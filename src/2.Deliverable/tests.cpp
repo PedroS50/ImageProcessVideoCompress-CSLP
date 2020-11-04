e160d9efb8e311e53e73bfa9d1582eeef2f3332e
Pedro <pedro.miguel50@ua.pt>
2020-11-04 15:08:22 +0000

    tests

--- src/2.Deliverable/tests.cpp
+++ src/2.Deliverable/tests.cpp
@@ -7,69 +7,65 @@ using namespace std;
 
 int main( int argc, char** argv) {
 	BitStream bs;
+
 	bs.setOutputFile("BinaryFile.bin");
+	
 	bs.setInputFile("BinaryFile.bin");
 
-	/*bs.writeStr("Hello World!");
-	vector<int> v = bs.readNBits(20);
-	for (int i = 0; i<v.size(); i++)
-		cout << v[i];
+	// --------------------
 
-	cout << "\n";*/
-	//bs.writeNBits(10101010);
-	/*int counter = 0;
-	while (true) {
-		counter++;
-		cout << bs.readBit();
-		if (counter == 10)
-			break;
-	}*/
-
-	/*bs.writeBit(1);
+	bs.writeBit(1);
 	bs.writeBit(1);
 	bs.writeBit(0);
-	bs.writeBit(0);
-	bs.writeBit(1);	
-	bs.writeBit(0);
-	bs.writeBit(0);
 	bs.writeBit(1);
-
-	bs.writeNBits(11001001);
-
-	bs.closeOutputFile();*/
-
-	for (int i = 0; i<5; i++){
-		cout << bs.readBit();
-	}
-
-	vector<int> v = bs.readNBits(3);
-	for (int i = 0; i<v.size(); i++)
-		cout << v[i];
-	cout << "\n";
 	
+	bs.writeNBits(1000);
+
+	bs.closeOutputFile();
+
+	// --------------------
+
+	vector <int> v = bs.readNBits(8);
+	for (int i = 0; i<v.size(); i++)
+		cout << v[i];
+	
+	cout << "\n";
+
+	// --------------------
+
+	bs.setOutputFile("BinaryFile.bin");
+
+	bs.writeNBits(11110010);
+
+	bs.closeOutputFile();
+
+	cout << bs.readBit();
+	cout << "\n";
+	cout << bs.readBit();
+	cout << "\n";
+	cout << bs.readBit();
+	cout << "\n";
+	cout << bs.readBit();
+	cout << "\n";
+	cout << bs.readBit();
+	cout << "\n";
+	cout << bs.readBit();
+	cout << "\n";
+	cout << bs.readBit();
+	cout << "\n";
+	cout << bs.readBit();
+	cout << "\n";
+	cout << "\n";
+
+	// --------------------
+
+	bs.resetRead();
+
 	v = bs.readNBits(8);
 	for (int i = 0; i<v.size(); i++)
 		cout << v[i];
+	
 	cout << "\n";
-
-	//bs.writeNBits(1110101111);
-	//bs.writeStr("Hello World!");
-	//bs.setInputFile("Whatever.bin");
-	/*
-	vector<int> v = bs.readNBits(3);
-	for (int i = 0; i<v.size(); i++)
-		cout << v[i];
-
-	cout << "\n";
-	cout << bs.readBit();
-	cout << bs.readBit();
-	cout << "\n";
-
-	v = bs.readNBits(3);
-	for (int i = 0; i<v.size(); i++)
-		cout << v[i];
-	cout << "\n";
-	*/
 	
 	return 0;
 }
