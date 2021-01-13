14042017debab46f332501cb69b04e3491315ebc
Pedro <pedro.miguel50@ua.pt>
2021-01-13 00:41:00 +0000

    cleanup

--- src/cleanupBuild.sh
+++ src/cleanupBuild.sh
@@ -5,6 +5,8 @@ rm BitStreamTests
 rm GolombTests
 rm IntraEncoder
 rm IntraframeEncoder_Class
+rm InterframeEncoder_Class
+rm HybridEncoder_Class
 rm InterEncoder
 rm HybridEncoder
 rm RealHybridEncoder
